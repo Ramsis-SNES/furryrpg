53a79ffc46e9545569054ecacf9281f987536bc0
Ramsis <ramsis@manuloewe.de>
2016-05-31 18:41:25 +0200

    Adapt build script for WLA DX v9.7b

--- make-it.bat
+++ make-it.bat
@@ -2,7 +2,7 @@
 calc_mode7_scaling_tables.py
 echo [objects] > furryrpg.lnk
 echo furryrpg.obj >> furryrpg.lnk
-wla-65816 -xo furryrpg.asm furryrpg.obj
-wlalink -rs furryrpg.lnk furryrpg.sfc
+wla-65816 -x -o furryrpg.obj furryrpg.asm
+wlalink -r -s furryrpg.lnk furryrpg.sfc
 del furryrpg.lnk
 pause
