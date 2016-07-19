af120babaa1dc4ca1b0fedef70bf9ff0ff898920
Ramsis <ramsis@manuloewe.de>
2016-07-19 18:30:36 +0200

    Add crude build/clean bash scripts

---
+++ make-it.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python calc_mode7_scaling_tables.py
+echo -e '[objects]\nfurryrpg.obj' >> furryrpg.lnk
+./wla-65816 -x -o furryrpg.obj furryrpg.asm
+./wlalink -r -s furryrpg.lnk furryrpg.sfc
+rm furryrpg.lnk
+echo 'Press any key to continue...'
+read -n 1 -s
