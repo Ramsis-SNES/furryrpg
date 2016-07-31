9478640597223ec68f1a0c5a3c56776cb62a8ace
Ramsis <ramsis@manuloewe.de>
2016-07-31 18:48:39 +0200

    Place assembler binaries in ~/local/bin

--- make-it.sh
+++ make-it.sh
@@ -2,8 +2,8 @@
 
 python calc_mode7_scaling_tables.py
 echo -e '[objects]\nfurryrpg.obj' >> furryrpg.lnk
-./wla-65816 -x -o furryrpg.obj furryrpg.asm
-./wlalink -r -s furryrpg.lnk furryrpg.sfc
+wla-65816 -x -o furryrpg.obj furryrpg.asm
+wlalink -r -s furryrpg.lnk furryrpg.sfc
 rm furryrpg.lnk
 echo 'Press any key to continue...'
 read -n 1 -s
