0ee140fa63ef1a874cbcd49f404bf01b070227f4
Ramsis-SNES <ramsis@manuloewe.de>
2018-08-25 17:40:48 +0200

    Remove obsolete Win batch script

--- make-it.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-calc_mode7_scaling_tables.py
-echo [objects] > furryrpg.lnk
-echo furryrpg.obj >> furryrpg.lnk
-wla-65816 -x -o furryrpg.obj furryrpg.asm
-wlalink -r -s furryrpg.lnk furryrpg.sfc
-del furryrpg.lnk
-pause
